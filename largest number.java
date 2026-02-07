0e60f59be6a7a73fc2e8548af71b00f7192cc6cb
Saideepak1806 <deepakyadavsai307@gmail.com>
2026-02-07 14:23:56 +0530

    Add files via upload

---
+++ largest number.java
@@ -0,0 +1,13 @@
+class largest{
+    public static void main(String[] args){
+        int A=90;
+    int B=65;
+
+    if(A>B)
+        System.out.println("A is greater than B");
+    else
+        System.out.println("B is greater than A");
+
+    }
+    
+}
---
+++ sum.java
@@ -0,0 +1,8 @@
+class sum{
+    public static void main(String[] args){
+        int a=19;
+        int b=35;
+        
+        System.out.println("sum="+(a+b));
+    }
+}
