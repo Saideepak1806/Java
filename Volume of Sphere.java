a7b383163b48a53497ac6558284991ae53485276
Saideepak1806 <deepakyadavsai307@gmail.com>
2026-02-09 13:33:57 +0530

    Add files via upload

---
+++ Volume of Sphere.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Volume{
+      public static void main(String[] args){
+           Scanner sc=new Scanner(System.in);
+           double r=sc.nextDouble();
+           double v=(4.0/3)*3.14*r*r*r;
+           System.out.println("Volume ="+v);
+          }
+        }
